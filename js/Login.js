7de885e4743d1ec4c48b3aadcbdacefa3de699c7
hazem-eltahan <hazem.a.eltahan@gmail.com>
2026-02-14 17:44:49 +0200

    Added login page

--- js/Login.js
+++ js/Login.js
@@ -1,3 +1,116 @@
+var login = document.getElementById("loginBtn");
+
+// console.log(login);
+
+var userEmail = document.getElementById("email");
+var userPassword = document.getElementById("password");
+
+// console.log(userPassword)
+
+var emailSpan = document.getElementById("email-span");
+var passwordSpan = document.getElementById("password-span");
+
+//console.log(emailSpan, passwordSpan);
+
+//-------------------------------------------------------------
+
+const fullNameRegex = /^[A-Za-z]{2,}( [A-Za-z]{2,})+$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const MessageEmpty = "This field is empty!";
+
+//-------------------------------------------------------------
+
+function validateEmail() {
+    var emailValue = userEmail.value.trim();
+    if (emailValue == "") {
+        emailSpan.textContent = MessageEmpty;
+        return 0;
+    }
+    else if (!emailRegex.test(emailValue)) {
+        emailSpan.textContent = "Invalid email!";
+        emailSpan.style.color = "red";
+        userEmail.style.borderColor = "red";
+        return 0;
+    }
+    else {
+        emailSpan.textContent = "";
+        userEmail.style.borderColor = "rgba(116, 235, 213, 0.8)";
+        return 1;
+    }
+}
+
+userEmail.addEventListener("input", function () { validateEmail(); })
+
+//----------------------------------------------------------------
+
+function validatePassword() {
+    var passwordValue = userPassword.value.trim();
+    if (passwordValue == "") {
+        passwordSpan.textContent = MessageEmpty;
+        return 0;
+    }
+    else if (!passwordRegex.test(passwordValue)) {
+        passwordSpan.textContent = "Invalid password Password must be 8+ chars, include uppercase, lowercase & number";
+        passwordSpan.style.color = "red";
+        userPassword.style.borderColor = "red";
+        return 0;
+    }
+    else{
+        passwordSpan.textContent = "";
+        userPassword.style.borderColor = "rgba(116, 235, 213, 0.8)";
+        return 1;
+    }
+}
+
+userPassword.addEventListener("input", function () {
+    validatePassword();
+})
+
+//----------------------------------------------------------------------
+var users =  [];
+
+function getDataFromLocalStorage(){
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+function validateLogin(e){
+    let loginState = validateEmail() && validatePassword();
+    e.preventDefault();
+
+    if(loginState){
 
 
+        users = getDataFromLocalStorage();
 
+        var flag = false;
+        if(users.length > 0){
+            for(var i = 0;i< users.length;i++){
+                if(users[i].email === userEmail.value){
+                    flag = true;
+                }
+            }
+
+        }
+        else{
+            //system has no users
+        }
+
+        if(flag== true)
+        {
+            setTimeout(() => {
+                location.replace("../html/Exam.html");
+            }, 1500);
+        }
+        else{
+            //no matching user
+        }
+
+    }
+
+}
+
+
+login.addEventListener("click", function (e) {
+    validateLogin(e);
+})
