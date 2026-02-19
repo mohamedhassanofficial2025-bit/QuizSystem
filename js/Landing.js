2f5cf108354ea795de62eedb6071ca1691adb6f8
mohamedhassanofficial2025-bit <mohamedhassan.official2025@gmail.com>
2026-02-20 00:34:35 +0200

    add gurad to landing page

--- js/Landing.js
+++ js/Landing.js
@@ -1,13 +1,17 @@
-const startBtn = document.getElementById('startBtn');
+if (!sessionStorage.getItem("UserGuard")) {
+    location.replace("../html/Login.html");
+} else { 
+    const startBtn = document.getElementById('startBtn');
 
-        startBtn.addEventListener('click', function() {
-        
-            if (!sessionStorage.getItem("ExamTime")) {
-                const duration = 10 * 60 * 1000; // 10 minutes
-                const endTime = Date.now() + duration;
-                sessionStorage.setItem("ExamTime", endTime);
-            }
+    startBtn.addEventListener('click', function () {
+
+        if (!sessionStorage.getItem("ExamTime")) {
+            const duration = 10 * 60 * 1000; // 10 minutes
+            const endTime = Date.now() + duration;
+            sessionStorage.setItem("ExamTime", endTime);
+        }
 
 
-            window.location.href = "../html/Exam.html";
-        });
+        window.location.href = "../html/Exam.html";
+    });
+}
